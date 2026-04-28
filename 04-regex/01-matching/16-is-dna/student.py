b95d1f976a38d77ea6493f8cb7e1643ebb81bf92
hannguy <hangng.data@gmail.com>
2026-04-28 22:15:22 +0200

    Exercises regex 21-is-valid-sstudent-id

--- 04-regex/01-matching/16-is-dna/student.py
+++ 04-regex/01-matching/16-is-dna/student.py
@@ -1,1 +1,4 @@
 # Write your code here
+import re
+def is_dna(string):
+    return re.fullmatch('[G|A|T|C]*', string)
--- 04-regex/01-matching/18-contains-a/student.py
+++ 04-regex/01-matching/18-contains-a/student.py
@@ -1,1 +1,3 @@
-# Write your code here
+import re
+def contains_a(string):
+    return re.search('a', string)
