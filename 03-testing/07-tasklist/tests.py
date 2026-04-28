41c5f97ca7121085b83878ea3761a177a137f5a9
hannguy <hangng.data@gmail.com>
2026-04-28 12:13:03 +0200

    exercise 11-fixtures replace setup_function with fixtures

--- 03-testing/07-tasklist/tests.py
+++ 03-testing/07-tasklist/tests.py
@@ -3,16 +3,27 @@ import pytest
 from tasks import *
 from calendars import *
 
-def setup_function():
-    global today, tomorrow, yesterday, calendar, sut
-    today = date(2000, 1, 1)
-    tomorrow = today + timedelta(days=1)
-    yesterday = today - timedelta(days=1)
-    calendar = CalendarStub(today)
-    sut = TaskList(calendar)
+@pytest.fixture
+def today():
+    return date(2000, 1, 1)
 
+@pytest.fixture
+def tomorrow(today):
+    return today + timedelta(days=1)
 
-def test_task_becomes_overdue():
+@pytest.fixture
+def yesterday(today):
+    return today - timedelta(days=1)
+
+@pytest.fixture
+def calendar(today):
+    return CalendarStub(today)
+
+@pytest.fixture
+def sut(calendar):
+    return TaskList(calendar)
+
+def test_task_becomes_overdue(sut, tomorrow, calendar):
     # Arrange
     next_week = date(2000, 1, 8)
     task = Task('description', tomorrow)
@@ -24,17 +35,14 @@ def test_task_becomes_overdue():
     # Assert
     assert [task] == sut.overdue_tasks
 
-def test_creation():
+def test_creation(sut):
     assert 0 == len(sut)
     assert [] == sut.due_tasks
     assert [] == sut.overdue_tasks
     assert [] == sut.finished_tasks
 
-def test_adding_task_with_due_day_in_future():
+def test_adding_task_with_due_day_in_future(tomorrow, sut):
     # Arrange
-    today = date(2026, 4, 28)
-    tomorrow = date(2026, 4, 29)
-
     task = Task('bake bread', tomorrow)
 
     # Action
@@ -44,7 +52,7 @@ def test_adding_task_with_due_day_in_future():
     assert 1 == len(sut)
     assert [task] == sut.due_tasks
 
-def test_adding_task_with_due_day_in_past():
+def test_adding_task_with_due_day_in_past(yesterday, sut):
     # Arrange
     task = Task('bake bread', yesterday)
 
@@ -52,7 +60,7 @@ def test_adding_task_with_due_day_in_past():
     with pytest.raises(RuntimeError):
         sut.add_task(task)
 
-def test_task_becomes_finished():
+def test_task_becomes_finished(tomorrow, sut):
     # Arrange
     task = Task('bake bread', tomorrow)
 
